c72fdb5e8484768939c7f467b04844f340ce444a
Khenysson <khenysson@hotmail.com>
2020-04-16 20:04:08 -0300

    Arquivos finais

--- mailer/config.php
+++ mailer/config.php
@@ -6,17 +6,17 @@
     $output 	= "Name: ".$name."\n\n Email: ".$email."\n\n Subject: ".$subject."\n\n Message: ".$message;
     //set your email address to receive messages
     $to 		= 'youremail@gmail.com';
-    $headers	= 'From: "'.$email.'"';
+    $headers	= 'Para: "'.$email.'"';
 
     // Send the email.
     if (mail($to, $name, $output, $headers)) {
         // Set a 200 (okay) response code.
         http_response_code(200);
-        echo "Thank You! Your message has been sent.";
+        echo "Que legaaaal!!! Sua mensagem foi enviada";
     } else {
         // Set a 500 (internal server error) response code.
         http_response_code(500);
-        echo "Oops! Something went wrong and we couldn't send your message.";
+        echo "Oops! Aconteceu algo de errado.";
     }
 
 ?>
